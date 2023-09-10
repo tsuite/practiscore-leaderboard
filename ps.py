e0df7ccd41cb830d423b22f53d27d8e734046940
tsuite <git@tsuite.net>
2023-09-10 23:18:38 +1000

    more

--- ps.py
+++ ps.py
@@ -1,16 +1,32 @@
 #!venv/bin/python3
 
-#import flask
+import flask
 import configobj
 import configobj.validate
 import asyncio
+import socket
+import uuid
+import time
+import struct
+import zlib
 
 import sys
 import json
 import argparse
 
+app = flask.Flask(__name__)
+
 __version__ = '1.0.0-alpha'
-print(__version__)
+print(f'practiscore-leaderboard-{__version__}')
+
+
+@app.get('/')
+def get_index():
+	return flask.render_template('index.html', matches=kiosk.matches(), devices=[1,2])
+
+@app.get('/json')
+def get_json():
+	return kiosk.data()
 
 class Kiosk:
 	def __init__(self):
@@ -30,17 +46,51 @@ class Kiosk:
 					error = 'missing value or section.'
 				raise ValueError (f'{config.filename}:{section_string}: {error}')
 		
+		if 'device_uuid' in config:
+			self.device_uuid = config['device_uuid']
+		else:
+			self.device_uuid = str(uuid.uuid4())
+		if 'match_uuid' in config:
+			self.match_uuid = config['match_uuid']
+		else:
+			self.match_uuid = str(uuid.uuid4())
+		self.division_name_substitutions = config.get('DivisionNameSubstitutions', {})
+		self.stage_name_substitutions = config.get('StageNameSubstitutions', {})
+		
 		self.devices = {}
 		if 'Devices' in config:
 			for device_name in config['Devices']:
-				self.devices[device_name] = PSDevice(device_name, config['Devices'][device_name])
+				self.devices[device_name] = PSDevice(device_name, config['Devices'][device_name], self.device_uuid, self.match_uuid)
 		if 'DummyDevices' in config:
 			for device_name in config['DummyDevices']:
 				self.devices[device_name] = JSONDevice(device_name, config['DummyDevices'][device_name])
 		
-		print(self.devices)
 		for device_name in self.devices:
-			asyncio.run(self.devices[device_name].update())
+			try:
+				#https://dev.to/jmarhee/recurring-background-jobs-in-flask-3407
+				asyncio.run(self.devices[device_name].update())
+			except Exception:
+				pass
+		for device_name in self.devices:
+			device = self.devices[device_name]
+			if device.match_def:
+				match_def = device.match_def
+	def data(self):
+		matches = self.matches()
+		return {'matches': [matches[id].data() for id in matches], 'devices': [self.devices[id].data() for id in self.devices]}
+	def matches(self):
+		matches = {}
+		for device_name in self.devices:
+			match_def = self.devices[device_name].match_def
+			match_scores = self.devices[device_name].match_scores
+			match_subtype = match_def.get('match_subtype', '')
+			match_id = match_def.get('match_id', '')
+			if match_id in matches:
+				matches[match_id].update(match_def, match_scores)
+			else:
+				if match_subtype == 'ipsc':
+					matches[match_id] = IPSCMatch(match_def, match_scores)
+		return matches
 
 class Device:
 	def __init__(self, name, config):
@@ -50,6 +100,9 @@ class Device:
 	
 	async def update(self):
 		raise NotImplementedError
+	
+	def data(self):
+		return {'name': self.name}
 
 class JSONDevice(Device):
 	def __init__(self, name, config):
@@ -73,47 +126,256 @@ class JSONDevice(Device):
 				pass
 
 class PSDevice(Device):
-	def __init__(self, name, config):
+	SIGNATURE = 0x19113006
+	FLAGS_ANDROID = 4
+	FLAGS_IOS = 3
+	#MSG_STATUS_REQUEST = 6
+	#MSG_STATUS_RESPONSE = 7
+	MSG_MATCH_REQUEST = 8
+	MSG_MATCH_RESPONSE = 9
+
+	def __init__(self, name, config, device_uuid, match_uuid):
 		super().__init__(name, config)
+		self.hostname = socket.gethostname()
 		self.address = config.get('address')
 		self.port = config.get('port')
 		self.timeout = config.get('timeout')
 		self.poll_time = config.get('poll_time')
 		self.slow_poll = config.get('slow_poll')
 		self.shutdown = config.get('shutdown')
-		self.status_path = config.get('status_path')
+		
 		self.match_def = {}
 		self.match_scores = {}
-		#asyncio.run(self.update())
 	
 	async def update(self):
+		await asyncio.wait_for(self.temp(), timeout=self.timeout)
+		
+	async def temp(self):
+		reader, writer = await asyncio.open_connection(self.address, self.port)
+		
+		tx_data = struct.pack('!IIIII', self.SIGNATURE, 0, self.MSG_MATCH_REQUEST, self.FLAGS_ANDROID, int(time.time()))
+		writer.write(tx_data)
+		await writer.drain()
+		
+		rx_header = await reader.readexactly(20)
+		
+		(f_signature, f_length, f_type, f_flags, f_time) = struct.unpack('!IIIII', rx_header)
+		f_signature_err = f_signature != self.SIGNATURE
+		f_type_err = f_type != self.MSG_MATCH_RESPONSE
+		f_flags_err = f_flags != self.FLAGS_ANDROID and f_flags != self.FLAGS_IOS
+		if f_signature_err or f_type_err or f_flags_err:
+			raise self.PSInvalidHeader
+		
+		match_def_length = struct.unpack('!I',await reader.readexactly(4))[0]
+		match_scores_length = f_length - match_def_length - 4
+		
+		match_def = json.loads(zlib.decompress(await reader.readexactly(match_def_length)))
+		if match_def:
+			self.match_def = match_def
+			if self.match_def_path:
+				with open(self.match_def_path, 'w') as f:
+					f.write(json.dumps(match_def))
+		if match_scores_length:
+			match_scores = json.loads(zlib.decompress(await reader.readexactly(match_scores_length)))
+			if match_scores:
+				self.match_scores = match_scores
+				if self.match_scores_path:
+					with open(self.match_scores_path, 'w') as f:
+						f.write(json.dumps(match_scores))
+		writer.close()
+		await writer.wait_closed()
+		
+	class PSInvalidHeader(Exception):
 		pass
 
 class Match:
-	pass
+	def __init__(self, match_def, match_scores):
+		self.match_id = match_def.get('match_id')
+		self.match_subtype = match_def.get('match_subtype')
+		self.shooters = {}
+		self.stages = {}
+		self.scores = {}
+		self.update_match_data(match_def)
+		self.update(match_def, match_scores)
+	
+	def update(self, match_def, match_scores):
+		match_modifieddate = match_def['match_modifieddate']
+		if match_modifieddate != self.match_modifieddate:
+			match_modifieddate_1 = datetime.datetime.strptime(match_modifieddate,'%Y-%m-%d %H:%M:%S.%f')
+			match_modifieddate_2 = datetime.datetime.strptime(self.match_modifieddate,'%Y-%m-%d %H:%M:%S.%f')
+			if match_modifieddate_1 > match_modifieddate_2:
+				self.update_match_data(match_def)
+		if 'match_shooters' in match_def:
+			self.update_shooters(match_def['match_shooters'])
+		if 'match_stages' in match_def:
+			self.update_stages(match_def['match_stages'])
+		if 'match_scores' in match_scores:
+			self.update_scores(match_scores['match_scores'])
+		self.match_name = match_def.get('match_name', '')
+		
+	def update_match_data(self, match_def):
+		self.match_name = match_def.get('match_name')
+		self.match_modifieddate = match_def.get('match_modifieddate')
+		
+	def update_scores(self, match_scores):
+		for stage in match_scores:
+			for stage_stagescore in stage['stage_stagescores']:
+				shooter_id = stage_stagescore['shtr']
+				stage_id = stage['stage_uuid']
+				if shooter_id in self.shooters:
+					if stage_id in self.shooters[shooter_id].scores:
+						self.shooters[shooter_id].scores[stage_id].update_if_modified(stage_stagescore)
+					elif self.match_subtype == 'scsa':
+							self.shooters[shooter_id].scores[stage_id] = SCSAStageScore(self, stage_id, stage_stagescore)
+					elif self.match_subtype == 'nra':
+							self.shooters[shooter_id].scores[stage_id] = NRAStageScore(self, stage_id, stage_stagescore)
+					elif self.match_subtype == 'ipsc':
+							self.shooters[shooter_id].scores[stage_id] = IPSCStageScore(self, stage_id, stage_stagescore)
+	
+	def update_stage(self, match_stage):
+		if match_stage['stage_uuid'] in self.stages:
+			self.stages[match_stage['stage_uuid']].update_if_modified(match_stage)
+		elif self.match_subtype == 'scsa':
+			self.stages[match_stage['stage_uuid']] = SCSAStage(self, match_stage)
+		elif self.match_subtype == 'nra':
+			self.stages[match_stage['stage_uuid']] = NRAStage(self, match_stage)
+		elif self.match_subtype == 'ipsc':
+			self.stages[match_stage['stage_uuid']] = IPSCStage(self, match_stage)
+		else:
+			self.stages[match_stage['stage_uuid']] = Stage(self, match_stage)
+	
+	def update_stages(self, match_stages):
+		for match_stage in match_stages:
+			self.update_stage(match_stage)
+	
+	def update_shooter(self, match_shooter):
+		if match_shooter['sh_uid'] in self.shooters:
+			self.shooters[match_shooter['sh_uid']].update_if_modified(match_shooter)
+		elif self.match_subtype == 'scsa':
+			self.shooters[match_shooter['sh_uid']] = SCSAShooter(match_shooter)
+		elif self.match_subtype == 'nra':
+			self.shooters[match_shooter['sh_uid']] = NRAShooter(match_shooter)
+		elif self.match_subtype == 'ipsc':
+			self.shooters[match_shooter['sh_uid']] = IPSCShooter(match_shooter)
+		else:
+			self.shooters[match_shooter['sh_uid']] = Shooter(match_shooter)
+	
+	def update_shooters(self, match_shooters):
+		for match_shooter in match_shooters:
+			self.update_shooter(match_shooter)
+	
+	def stage_list(self):
+		list = [id for id in self.stages if not self.stages[id].stage_deleted]
+		return sorted(list, key=lambda x: (self.stages[x].stage_number, self.stages[x].id))
+	def shooter_data(self):
+		return [self.shooters[id].data() for id in self.shooters]
+	def stage_data(self):
+		return [self.stages[id].data() for id in self.stages]
 
 class IPSCMatch(Match):
-	pass
+	def update_match_data(self, match_def):
+		super().update_match_data(match_def)
+		self.match_pfs = match_def.get('match_pfs')
+	def data(self):
+		return {'match_name': self.match_name, 'match_pfs': self.match_pfs, 'stages':[self.stages[id].data() for id in self.stages]}
 
 class Shooter:
-	pass
+	def __init__(self, match_shooter):
+		self.id = match_shooter['sh_uid']
+		self.update(match_shooter)
+		self.scores = {}
+	
+	def update_if_modified(self, match_shooter):
+		modified_date = match_shooter['sh_mod']
+		if match_shooter['sh_mod'] != self.modified_date:
+			modified_time_1 = datetime.datetime.strptime(modified_date,'%Y-%m-%d %H:%M:%S.%f')
+			modified_time_2 = datetime.datetime.strptime(self.modified_date,'%Y-%m-%d %H:%M:%S.%f')
+			if modified_time_1 > modified_time_2:
+				self.update(match_shooter)
+	
+	def update(self, match_shooter):
+		self.firstname = match_shooter['sh_fn']
+		self.lastname = match_shooter['sh_ln']
+		self.division = match_shooter['sh_dvp']
+		if self.division in kiosk.division_name_substitutions:
+			self.short_division = kiosk.division_name_substitutions[match_shooter['sh_dvp']]
+		else:
+			self.short_division = self.division
+		self.deleted = match_shooter.get('sh_del', False)
+		self.disqualified = match_shooter.get('sh_dq', False)
+		self.modified_date = match_shooter['sh_mod']
+	
+	def name(self):
+		return f'{self.firstname} {self.lastname}'
 
 class IPSCShooter(Shooter):
-	pass
+	def score(self, stage):
+		if not self.disqualified and stage.id in self.scores:
+			score = self.scores[stage.id]
+			if score.hit_factor != 0 and not score.dnf:
+				return score.hit_factor
+	
+	def score_string(self, stage):
+		if not self.disqualified and stage.id in self.scores:
+			score = self.scores[stage.id]
+			if score.dnf:
+				return 'DNF'
+			if score.hit_factor != 0:
+				return score.hit_factor_string
+		return '-'
+	
+	def total(self, stages):
+		return 0
+	
+	def total_string(self, stages):
+		return '-'
+	
+	def data(self):
+		return {'name': self.name(), 'short_division': self.short_division}
 
 class Stage:
-	pass
+	def __init__(self, match, match_stage):
+		self.match = match
+		self.id = match_stage['stage_uuid']
+		self.update(match_stage)
+		
+	def update_if_modified(self, match_stage):
+		stage_modifieddate = match_stage['stage_modifieddate']
+		if stage_modifieddate != self.stage_modifieddate:
+			stage_modifieddate_1 = datetime.datetime.strptime(stage_modifieddate,'%Y-%m-%d %H:%M:%S.%f')
+			stage_modifieddate_2 = datetime.datetime.strptime(self.stage_modifieddate,'%Y-%m-%d %H:%M:%S.%f')
+			if stage_modifieddate_1 > stage_modifieddate_2:
+				self.update(match_stage)
+	
+	def update(self, match_stage):
+		self.stage_number = match_stage.get('stage_number')
+		self.stage_name = match_stage.get('stage_name')
+		self.stage_modifieddate = match_stage.get('stage_modifieddate')
+		self.stage_deleted = match_stage.get('stage_deleted', False)
 
 class IPSCStage(Stage):
-	pass
+	def update(self, match_stage):
+		super().update(match_stage)
+		self.stage_poppers = match_stage.get('stage_poppers', 0)
+		self.stage_targets = match_stage.get('stage_targets', [])
+	
+	def max_points(self):
+		return 5*(self.stage_poppers + sum(stage_target.get('target_reqshots', 0) for stage_target in self.stage_targets))
+	
+	def data(self):
+		return {'stage_id': self.id, 'stage_number': self.stage_number, 'max_points': self.max_points(), 'stage_poppers': self.stage_poppers, 'stage_targets': self.stage_targets, 'stage_deleted': self.stage_deleted}
 
 class StageScore:
-	pass
+	def __init__(self, match, stage_id, stage_stagescore):
+		pass
 
 class IPSCStageScore(StageScore):
 	pass
 
 kiosk = Kiosk()
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
 
 exit()
 
